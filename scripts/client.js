e0a1a6e0cd600e3ffaa310370d06d62a42b94f7a
Myron <dubtwee@gmail.com>
2019-11-18 10:31:42 -0600

    rendering the new employee entered and the total monthly salary

--- scripts/client.js
+++ scripts/client.js
@@ -7,6 +7,21 @@ $(document).ready(init)
 function init() {
     // capture submit click event
     $('.js-submit').on('click', onClickSubmit);
+    $('.js-employee-table').on('click', '.js-btn-delete', onClickDelete);
+}
+
+function calculateMonthly() {
+    let totalMonthlySalary = 0;
+
+    for (let i = 0; i < EMPLOYEE_LIST.length; i++) {
+        const indvEmployee = EMPLOYEE_LIST[i];
+        const annualSalary = indvEmployee.annualSalary;
+        const monthlySalary = annualSalary / 12;
+
+        totalMonthlySalary += monthlySalary;
+    }
+
+    return totalMonthlySalary;
 }
 
 //
@@ -24,6 +39,45 @@ function onClickSubmit(event) {
 
     EMPLOYEE_LIST.push(newEmployee);
     console.log('Clicked Submit - EMPLOYEE_LIST: ', EMPLOYEE_LIST);
+    render();
+}
+
+function onClickDelete(event) {
+    const index = $(event.target).data('index');
+    // const index = event.target.dataset.index;
+    EMPLOYEE_LIST.splice(index, 1);
+    console.log('EMPLOYEE_LIST: ', EMPLOYEE_LIST);
+    render();
 }
 
 // render content to DOM
+function render() {
+    const totalMonthly = calculateMonthly();
+    const $employeeTable = $('.js-employee-table');
+    const $monthly = $('.js-total-monthly');
+
+    $employeeTable.empty();
+
+    for (let i = 0; i < EMPLOYEE_LIST.length; i++) {
+        const indvEmployee = EMPLOYEE_LIST[i];
+        $employeeTable.append(`
+            <tr>
+                <td>${indvEmployee.firstName}</td>
+                <td>${indvEmployee.lastName}</td>
+                <td>${indvEmployee.id}</td>
+                <td>${indvEmployee.title}</td>
+                <td>$${indvEmployee.annualSalary}</td>
+                <td><button class="js-btn-delete" data-index="${i}">DELETE</button></td>
+            </tr>
+        `);
+    }
+
+    let monthlyStyle = 'fair';
+    if (totalMonthly > 20000) {
+        monthlyStyle = 'red';
+    }
+
+    $monthly.empty();
+    $monthly.append('<div class="' + monthlyStyle + '">$' + totalMonthly + '</div>');
+
+}
